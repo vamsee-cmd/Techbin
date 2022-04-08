ddf1dc972fdf69d50a80bd5fe10ad4f2235be5d2
vamsee-cmd <vamshi.embadi1234@gmail.com>
2022-04-08 20:51:03 +0530

    Revert "camera size"
    This reverts commit bf6ca31cdf845e08aaee4a75470e0bd00b7a5fff.

--- updatePoints.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$conn = new mysqli('remotemysql.com', 'WK1eggxcgK', 'Q7gkaeCv2k', 'WK1eggxcgK');
-if ($conn->connect_error) {
- die("Connection failed: " . $conn->connect_error);
-}
-$sql="update techbin set points=".$_SESSION["points"]." where email='".$_SESSION['user']."'";
-$conn->query($sql);
-?>
