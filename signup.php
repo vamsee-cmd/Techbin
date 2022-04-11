e46f6964404ca49ef7ccae8e73ba96518759e944
vamsee-cmd <81890751+vamsee-cmd@users.noreply.github.com>
2022-04-11 15:04:40 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -16,7 +16,7 @@
     <?php
     if(isset($_POST['email'])){
     if($_POST['pass']==$_POST["cpass"]){
-    $conn = new mysqli('remotemysql.com', 'WK1eggxcgK', 'Q7gkaeCv2k', 'WK1eggxcgK');
+    $conn = new mysqli('remotemysql.com', 'WK1eggxcgK', 'z9HcRqhc5i', 'WK1eggxcgK');
     if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
     }
